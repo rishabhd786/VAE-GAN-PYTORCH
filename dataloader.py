279c1b92af1afdc370c96a560b7bae2f0dd9c777
Rishabh Dugaye <58358984+rishabhd786@users.noreply.github.com>
2020-05-18 20:30:59 +0530

    Update dataloader.py

--- dataloader.py
+++ dataloader.py
@@ -5,8 +5,9 @@ import torchvision.transforms as transforms
 import torchvision.utils as vutils
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-batch_size=64
-dataroot="/content/drive/My Drive/celeba"
-transform=transforms.Compose([ transforms.Resize(64),transforms.CenterCrop(64),transforms.ToTensor(),transforms.Normalize((0.5),(0.5))])
-dataset=torchvision.datasets.MNIST(root=dataroot, train=True,transform=transform, download=True)
-data_loader = torch.utils.data.DataLoader(dataset, batch_size=batch_size, shuffle=True)
+def dataloader(batch_size):
+  dataroot="/content/drive/My Drive/celeba"
+  transform=transforms.Compose([ transforms.Resize(64),transforms.CenterCrop(64),transforms.ToTensor(),transforms.Normalize((0.5),(0.5))])
+  dataset=torchvision.datasets.MNIST(root=dataroot, train=True,transform=transform, download=True)
+  data_loader = torch.utils.data.DataLoader(dataset, batch_size=batch_size, shuffle=True)
+  return data_loader
